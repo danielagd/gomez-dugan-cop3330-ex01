d95e553da7e65c2f2340fc5a2ef51b897d39de68
Daniela <danigomezdugan@gmail.com>
2021-09-07 16:43:28 -0400

    Updating file

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,15 +1,16 @@
 package org.example;
 import java.util.Scanner;
+
 /**
  * UCF COP3330 Fall 2021 Assignment 1 Solution
- *Copyright 2021 daniela gomez-dugan
+ *Copyright 2021 Daniela Gomez-Dugan
  */
-public class hello
+public class App 
 {
-    public static void main(String[] args )
+    public static void main( String[] args )
     {
         Scanner input = new Scanner(System.in);
-        System.out.println( "What is your name?");
+        System.out.print("What is your name? ");
         String name = input.nextLine();
         System.out.println("Hello, " + name + ", nice to meet you!");
     }
