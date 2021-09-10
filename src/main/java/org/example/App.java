a638db4899735e61379d09e4d7c0f848f87838cc
Daniela <danigomezdugan@gmail.com>
2021-09-09 20:47:03 -0400

    Updating file

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,8 +10,11 @@ public class App
     public static void main( String[] args )
     {
         Scanner input = new Scanner(System.in);
+
+        // Ask user for their name
         System.out.print("What is your name? ");
         String name = input.nextLine();
+
         System.out.println("Hello, " + name + ", nice to meet you!");
     }
 }
